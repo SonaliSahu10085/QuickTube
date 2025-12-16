c561bdce3ea3e4750c5fe73996f1963bfff16233
Sonali Sahu <sonalisahu0064@gmail.com>
2025-12-16 10:20:18 +0530

    Added cron job

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-require('dotenv/config');
+require("dotenv/config");
 const express = require("express");
-const path = require('path')
+const path = require("path");
+const job = require("./lib/cron");
+
 const app = express();
-
-
+job.start();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -11,9 +12,8 @@ app.set("view engine", "ejs");
 
 app.use(express.static(path.join(__dirname, "public")));
 
-
 app.get("/", (req, res) => {
-  res.render('index.ejs');
+  res.render("index.ejs");
 });
 
 app.listen(3000, () => console.log("Server is listening on port 3000"));
